2339ca85d9e5ebb07b0fb7403ab4d02839bb75b2
SvenAlHamad <sven@webiny.com>
2015-02-05 11:44:13 +0000

    Version bumped to 1.2.2

--- StdObject/StdObjectAbstract.php
+++ StdObject/StdObjectAbstract.php
@@ -22,11 +22,6 @@ abstract class StdObjectAbstract implements StdObjectInterface
 {
     use ValidatorTrait;
 
-    /**
-     * ArrayObject that caches the names of standard objects. This is used by StdObjectAbstract::_getStdObjectName
-     * @var ArrayObject
-     */
-    private static $_stdObjectName = null;
 
     /**
      * Return, or update, current standard objects value.
@@ -66,50 +61,5 @@ abstract class StdObjectAbstract implements StdObjectInterface
     public function exception($message)
     {
         return new StdObjectException($message);
-    }
-
-    /**
-     * Returns the name of standard object config class, or throws a StdObjectException if config class does not exist.
-     *
-     * @return string
-     * @throws StdObjectException
-     */
-    private static function _getStdObjectConfigClassName()
-    {
-        $stdObjectName = self::_getStdObjectName();
-        $configClassName = 'Webiny\Component\StdLib\StdObject\\' . $stdObjectName . '\\' . $stdObjectName . 'Config';
-        if (!self::classExists($configClassName)) {
-            throw new StdObjectException('StdObjectAbstract: Config class for "' . $stdObjectName . '" standard object does not exist.'
-            );
-        }
-
-        return $configClassName;
-    }
-
-    /**
-     * Returns the name of current standard object without its namespace.
-     *
-     * @return ArrayObject|StringObject
-     */
-    private static function _getStdObjectName()
-    {
-        // check if self::$_stdObjectName is created
-        if (self::isNull(self::$_stdObjectName)) {
-            self::$_stdObjectName = new ArrayObject([]);
-        }
-
-        // get called class name (with full namespace)
-        $cc = get_called_class();
-
-        // check if we already have an entry for this class name
-        if (!self::$_stdObjectName->keyExists($cc)) {
-            $str = new StringObject($cc);
-            $className = $str->explode('\\')->last();
-            self::$_stdObjectName->key($cc, $className);
-        } else {
-            $className = self::$_stdObjectName->key($cc);
-        }
-
-        return $className;
     }
 }
